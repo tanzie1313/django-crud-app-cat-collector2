d01b7d89a90845bd0f55ac016e0b306e640a0007
Tanzie <tanziehickory@gmail.com>
2025-04-17 23:00:10 -0700

    inital plus

---
+++ main_app/views.py
@@ -0,0 +1,33 @@
+# main_app/views.py
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+from django.shortcuts import render
+
+# Define the home view function
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
+
+# C# views.py
+
+
