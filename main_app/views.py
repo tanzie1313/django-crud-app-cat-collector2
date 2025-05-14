cec94eb5bf2fe7af301f6fd78d0402fd48c6e850
Tanzie <tanziehickory@gmail.com>
2025-05-14 10:37:09 -0700

    feat all cats

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@
 
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 # Define the home view function
 def home(request):
     return render(request, 'home.html')
@@ -17,6 +19,18 @@ def cat_index(request):
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
 
 # C# views.py
 
